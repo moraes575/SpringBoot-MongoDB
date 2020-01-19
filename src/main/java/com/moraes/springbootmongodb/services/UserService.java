691fd60343dd119b3105c51bcac65550b38bacb6
Matheus Moraes <matheus.moraes111@outlook.com>
2020-01-19 14:43:44 -0300

    Atualização de usuário com PUT

--- src/main/java/com/moraes/springbootmongodb/services/UserService.java
+++ src/main/java/com/moraes/springbootmongodb/services/UserService.java
@@ -11,30 +11,41 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class UserService {
-
+    
     @Autowired
     private UserRepository repo;
-
+    
     public List<User> findAll() {
         return repo.findAll();
     }
-
+    
     public User findById(String id) {
         Optional<User> obj = repo.findById(id);
         return obj.orElseThrow(() -> new ObjectNotFoundException("Object not found"));
     }
-
+    
     public User insert(User obj) {
         return repo.insert(obj);
     }
-
+    
     public void delete(String id) {
         findById(id);
         repo.deleteById(id);
     }
-
+    
+    public User update(User obj) {
+        User newObj = findById(obj.getId());
+        updateData(newObj, obj);
+        return repo.save(newObj);
+    }
+    
     public User fromDTO(UserDTO objDTO) {
         return new User(objDTO.getId(), objDTO.getName(), objDTO.getEmail());
     }
-
+    
+    public void updateData(User newObj, User obj) {
+        newObj.setName(obj.getName());
+        newObj.setEmail(obj.getEmail());
+    }
+    
 }
