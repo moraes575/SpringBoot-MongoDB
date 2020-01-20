f765d14ee1ba480ac0e3bd6602f12db88e79be34
Matheus Moraes <matheus.moraes111@outlook.com>
2020-01-20 14:46:15 -0300

    Endpoint para retornar os posts de um usuário

--- src/main/java/com/moraes/springbootmongodb/resources/UserResource.java
+++ src/main/java/com/moraes/springbootmongodb/resources/UserResource.java
@@ -1,5 +1,6 @@
 package com.moraes.springbootmongodb.resources;
 
+import com.moraes.springbootmongodb.domain.Post;
 import com.moraes.springbootmongodb.domain.User;
 import com.moraes.springbootmongodb.dto.UserDTO;
 import com.moraes.springbootmongodb.services.UserService;
@@ -55,6 +56,12 @@ public class UserResource {
         obj.setId(id);
         obj = service.update(obj);
         return ResponseEntity.noContent().build();
+    }
+
+    @RequestMapping(value = "/{id}/posts", method = RequestMethod.GET)
+    public ResponseEntity<List<Post>> findPosts(@PathVariable String id) {
+        User obj = service.findById(id);
+        return ResponseEntity.ok().body(obj.getPosts());
     }
 
 }
